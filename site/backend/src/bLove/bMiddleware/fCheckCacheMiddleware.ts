5637cd82eeaf45704e34dcd1e6e98faedafb2f48
Apurv Chatur <official.apurv.chatur@gmail.com>
2025-09-20 03:30:29 +0530

    XXX-013: Create Middleware (With Errors)

---
+++ site/backend/src/bLove/bMiddleware/fCheckCacheMiddleware.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+
+import { redisClient } from '../../aConnection/eRedisConnection';
+import catchAsyncMiddleware from "./bCatchAsyncMiddleware";
+
+type TCheckCacheOptions = {
+  key: string;
+  label: string;
+  name: string;
+  customMessage?: string;
+};
+
+const checkCacheMiddleware = ({ key, label, name, customMessage }: TCheckCacheOptions) => (
+  catchAsyncMiddleware(
+    // Returning Function
+    async (request: express.Request, response: express.Response, next: express.NextFunction) => {
+
+      // Check Cache
+      const cacheKey = 
+        (key === `${label}-retrieve`) ? (`${label}-retrieve:${request.params.id}`) : 
+        (key);
+
+      const oldCache = await redisClient.get(cacheKey);
+
+      if (oldCache) {
+        console.log("Congratulations... We got cache");
+
+        // Response
+        response.status(200).json({
+          success: true,
+          message: customMessage || `${label} ${name}ed Successfully... From Backend Cache`,
+          [name]: JSON.parse(oldCache),
+        });
+
+        return;
+      } else {
+        console.log("Oops... No cache found");
+
+        // Move Next
+        return next();
+      } 
+    }
+  )
+);
+
+export default checkCacheMiddleware;
