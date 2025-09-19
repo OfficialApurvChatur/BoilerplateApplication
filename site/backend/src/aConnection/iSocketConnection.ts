762631e54a251617bdc9ecc14c4f45642ce978c6
Apurv Chatur <official.apurv.chatur@gmail.com>
2025-09-19 17:43:17 +0530

    XXX-007: aConnection setup completed

---
+++ site/backend/src/aConnection/iSocketConnection.ts
@@ -0,0 +1,61 @@
+import http from "http";
+import express from "express";
+import { Server } from "socket.io";
+import loggerConnection from "./bLoggerConnection";
+
+
+const socketConnection = (server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>, appConnection: express.Express) => {
+  try {
+    // Socket 
+    const io = new Server(server, {
+      cors: { 
+        origin: "*", 
+        credentials: true 
+      }
+    })
+
+    // Set Socket in appConnectionConnection
+    appConnection.set("io", io);
+
+    // Listening Event
+    io.on("connection", (socket: any) => {
+      try {
+        loggerConnection().debug({ 
+          message: "✅ Great... Socket Connected",
+          context: {
+            socket: socket.id,
+          }
+        });
+
+        socket.on("disconnect", () => {
+          try {
+            loggerConnection().debug({ 
+              message: "✅ Great... Socket Disconnected",
+              context: {
+                socket: socket.id,
+              }
+            });
+          } catch (error: any) {
+            loggerConnection().error({ 
+              message: "❌ Socket Disconnection Error",
+              context: { error } 
+            });
+          }
+        })
+
+      } catch (error: any) {
+        loggerConnection().error({ 
+          message: "❌ Socket Connection Error",
+          context: { error } 
+        });
+      }
+    })  
+  } catch (error: any) {
+    loggerConnection().error({ 
+      message: "❌ Socket Critical Error",
+      context: { error } 
+    });
+  }
+}
+
+export default socketConnection;
