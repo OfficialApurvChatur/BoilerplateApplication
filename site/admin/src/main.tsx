1ffa4c3548c161da48e3dfd9ff91f76502744fef
Apurv Chatur <official.apurv.chatur@gmail.com>
2025-10-01 15:58:27 +0530

    XXX-021: hello frontend setup completed

--- site/admin/src/main.tsx
+++ site/admin/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './aConnection/bShadcnConnection/index.css'
 import AppConnection from './aConnection/aAppConnection'
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppConnection />
