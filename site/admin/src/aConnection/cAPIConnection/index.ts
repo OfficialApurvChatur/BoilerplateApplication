c16b3a83b7d6698309feff8e899bd1ed9b25c7e0
Apurv Chatur <official.apurv.chatur@gmail.com>
2025-10-29 19:04:52 +0530

    XXX-043: fixed cross origin urls

--- site/admin/src/aConnection/cAPIConnection/index.ts
+++ site/admin/src/aConnection/cAPIConnection/index.ts
@@ -5,8 +5,12 @@ import brandConnection from "../eBrandConnection";
 const apiConnection = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${brandConnection.oBackendBaseURL}/api/v1`,
-    credentials: "include"
+    baseUrl: `${brandConnection.oBackendBaseURL.replace(/\/$/, "")}/api/v1`,
+    credentials: "include",
+    prepareHeaders: (headers) => {
+      headers.set("Accept", "application/json");
+      return headers;
+    }
   }),
   tagTypes: [
     "adminHomePageRetrieve"               , 
